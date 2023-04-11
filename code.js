52c4bb9845a37001868b061ec00f9c5fee88d095
Dario Cruz <rio2012@gmail.com>
2023-04-11 13:49:52 -0400

    Add events to check for validation, add required att to all input elems, change select element so blank options have innter text.

--- code.js
+++ code.js
@@ -8,3 +8,29 @@ const inputCountry = document.querySelector('#country-input')
 const inputZip = document.querySelector('#zip-input')
 const inputPass = document.querySelector('#pass-input')
 const inputPassConf =document.querySelector('#pass-confirm')
+
+
+// Live check first and last name input and through error if pattern is not matched.
+inputFirstName.addEventListener('input', (e) => {
+    if (inputFirstName.validity.patternMismatch) {
+        inputFirstName.setCustomValidity('Names may only consist of letters and needed Punctuation.')
+    } else {
+        inputFirstName.setCustomValidity('')
+    }
+})
+inputLastName.addEventListener('input', (e) => {
+    if(inputLastName.validity.patternMismatch) {
+        inputLastName.setCustomValidity('Names may only consist of letters and needed Punctuation.')
+    } else {
+        inputLastName.setCustomValidity('')
+    }
+})
+
+// Check that the zip code is only 5 digits long if not throw error.
+inputZip.addEventListener('input', (e) => {
+    if(inputZip.validity.patternMismatch) {
+        inputZip.setCustomValidity('5 Digit Zip code only.')
+    } else {
+        inputZip.setCustomValidity('')
+    }
+})
