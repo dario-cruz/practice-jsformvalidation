f9ced73adeef98704fce5cb6de14c9f130bc1cdd
Dario Cruz <rio2012@gmail.com>
2023-04-11 13:19:08 -0400

    Define all elements needed for js code.

---
+++ code.js
@@ -0,0 +1,10 @@
+// Define all needed elements from page or form.
+const formElement = document.querySelector('#page-form')
+const inputLastName = document.querySelector('#lname-input')
+const inputFirstName = document.querySelector('#fname-input')
+const inputEmail = document.querySelector('#email-input')
+const serviceSelect = document.querySelector('#service-select')
+const inputCountry = document.querySelector('#country-input')
+const inputZip = document.querySelector('#zip-input')
+const inputPass = document.querySelector('#pass-input')
+const inputPassConf =document.querySelector('#pass-confirm')
